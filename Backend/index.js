fc0f1b0c046174b893074ccb6fb9e7ba14c01712
Ryan <ryansaftje123@gmail.com>
2024-05-30 10:32:36 +0700

    test

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,14 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: 'https://game-review-full.vercel.app', // Replace with your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
+  
+  // Handle preflight requests
+  app.options('*', cors());
 
 connectDB.connectDB();
 app.use(routes);
